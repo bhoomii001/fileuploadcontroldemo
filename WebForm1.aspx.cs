e1c856b9efc828669ffe0116f609b078e20d2e29
Bhoomi <bhoomiahir321@gmail.conm>
2025-01-15 15:30:43 +0530

    fileuppload program added

---
+++ WebForm1.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace fileuploadcontroldemo
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string path;
+            string file = FileUpload1.FileName;
+            path=Server.MapPath("~\\FILES\\")+file;
+            FileUpload1.SaveAs(path);
+            Label1.Text = "FILE UPLOADED";        
+        }
+    }
+}
